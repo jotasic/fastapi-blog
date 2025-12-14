ad33c1323d8d303b9c2acc84b0507a7ce03c6c0e
taewookim <embedded61@gmail.com>
2025-12-15 08:58:37 +0900

    add: 프로젝트 환경변수 로드 구축

---
+++ app/config.py
@@ -0,0 +1,49 @@
+# https://fastapi.tiangolo.com/advanced/settings/#pydantic-settings
+from typing import Literal
+
+from pydantic import PostgresDsn, computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_ignore_empty=True,
+        extra="ignore",
+    )
+
+    ENVIRONMENT: Literal["local", "prod"] = "local"
+
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_DB: str = ""
+    POSTGRES_TEST_DB: str = "test_database"
+
+    @computed_field
+    @property
+    def DATABASE_URI(self) -> str:  # noqa: N802
+        return PostgresDsn.build(
+            scheme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        ).encoded_string()
+
+    @computed_field
+    @property
+    def DATABASE_TEST_URI(self) -> str:  # noqa: N802
+        return PostgresDsn.build(
+            scheme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_TEST_DB,
+        ).encoded_string()
+
+
+settings = Settings()
