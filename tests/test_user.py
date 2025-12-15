f14e3b8ac5db350e5dde00ffa8f0c3019360fd86
taewookim <embedded61@gmail.com>
2025-12-15 10:57:37 +0900

    Modify: user 모델 변경 사항 테스트에 반영

--- tests/test_user.py
+++ tests/test_user.py
@@ -9,15 +9,15 @@ if TYPE_CHECKING:
 
 # TODO: 해당 테스트는 Test DB 셋업이 잘 되는지 확인하기 위함이므로 추후 실 Test 수정
 def test_create_user(session: Session):
-    name = random_lower_string(User.name.type.length)
+    nickname = random_lower_string(User.nickname.type.length)
     email = random_email()
 
-    user = User(name=name, email=email)
+    user = User(nickname=nickname, email=email, hashed_password="")
     session.add(user)
     session.commit()
     session.refresh(user)
 
     db_user = session.get(User, user.id)
 
-    assert db_user.name == name
+    assert db_user.nickname == nickname
     assert db_user.email == email
