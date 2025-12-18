c44707c77e21ee9bc14648d61fc8502b9e410e0a
taewookim <embedded61@gmail.com>
2025-12-18 14:53:31 +0900

    Modify: user model 수정 - created_at과 updated_at이 동일하지 않은 현상 이벤트 리스너로 일치 시기도록 수정 - server default 값을 추가하여 ddl에서도 볼 수 있도록 추가

--- app/models.py
+++ app/models.py
@@ -1,29 +1,47 @@
-from datetime import datetime
+from datetime import UTC, datetime
 
-from sqlalchemy import String
+from sqlalchemy import DateTime, Integer, String, event, false, null
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql import func
 
 
 class BaseModel(DeclarativeBase):
     pass
 
 
-# https://docs.sqlalchemy.org/en/20/orm/declarative_mixins.html#mixing-in-columns
 class TimestampMixin:
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now(), nullable=False)
-    updated_at: Mapped[datetime] = mapped_column(default=datetime.now(), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        onupdate=lambda: datetime.now(UTC),
+        server_default=func.now(),
+    )
+
+
+# 생성 시, create_at과 update_at에 동일한 값을 넣기 위한 작업
+# 각각 default=lambda: datetime.now(UTC) 설정 시, 각각 호출하므로 값이 미묘하게 달라짐
+@event.listens_for(TimestampMixin, "before_insert", propagate=True)
+def setup_default_timestamps(mapper, connection, target):
+    print("setup_timestamps ca")
+    current = datetime.now(UTC)
+    if not target.created_at:
+        target.created_at = current
+        target.updated_at = current
+
+    if not target.updated_at:
+        target.updated_at = target.created_at
 
 
 class SoftDeleteMixin:
-    is_delete: Mapped[bool] = mapped_column(default=False, nullable=False)
-    deleted_at: Mapped[datetime] = mapped_column(default=None, nullable=True)
+    is_delete: Mapped[bool] = mapped_column(default=False, server_default=false())
+    deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), default=None, server_default=null())
 
 
 class User(SoftDeleteMixin, TimestampMixin, BaseModel):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(320), unique=True, nullable=False)
-    hashed_password: Mapped[str] = mapped_column(String(250), nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(320), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(250))
     nickname: Mapped[str] = mapped_column(String(30))
-    is_active: Mapped[bool] = mapped_column(default=False, nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=False, server_default=false())
