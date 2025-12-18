22f1938c1b23b522aa4ad956b0e9389fd86b23b5
taewookim <embedded61@gmail.com>
2025-12-19 00:39:16 +0900

    Feat: session fixture 수정 및 test 편의성을 위한 fixture 추가 - 함수별로 db 변동 사항을 롤백시켜서 테스트간 격리성 보장 - 기본 user_token 생성을 위한 픽스쳐 생성 - 위 픽스쳐 수정을 위해 필요한 테스트 유틸함수 추가

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,18 @@
 import random
 import string
+from typing import TYPE_CHECKING
+
+from app.core.security import create_password_hash
+from app.models import User
+
+if TYPE_CHECKING:
+    from fastapi.testclient import TestClient
+    from sqlalchemy.orm import Session
+
+
+DEFAULT_USER_EMAIL = "example@test.com"
+DEFAULT_USER_NICKNAME = "user"
+DEFAULT_USER_PASSWORD = "Test1234!"
 
 
 def random_lower_string(length: int = 32) -> str:
@@ -8,3 +21,27 @@ def random_lower_string(length: int = 32) -> str:
 
 def random_email() -> str:
     return f"{random_lower_string()}@{random_lower_string()}.com"
+
+
+def create_default_user(session: Session):
+    user = User(
+        nickname=DEFAULT_USER_NICKNAME,
+        email=DEFAULT_USER_EMAIL,
+        hashed_password=create_password_hash(DEFAULT_USER_PASSWORD),
+    )
+    session.add(user)
+    session.commit()
+
+
+def init_test_data(session: Session):
+    create_default_user(session)
+
+
+def get_user_token(*, client: TestClient, email: str, password: str) -> dict[str, str]:
+    data = {"username": email, "password": password}
+
+    r = client.post("/v1/auth/login", data=data)
+    response = r.json()
+    auth_token = response["access_token"]
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    return headers
