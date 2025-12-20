557b1366483cdbddd93f4beddebfcbba2384b294
taewookim <embedded61@gmail.com>
2025-12-21 00:24:23 +0900

    Add: 포스트 댓글 기능 관련 API 및 라우터 추가

---
+++ app/api/routes/post_comment.py
@@ -0,0 +1,72 @@
+from datetime import UTC, datetime
+from typing import Annotated
+
+from fastapi import APIRouter, HTTPException, Query, Response, status
+
+from app import query
+from app.api.dependency import AuthUserDep  # noqa: TC001
+from app.database import SessionDep  # noqa: TC001
+from app.filters import PostCommentFilterParams  # noqa: TC001
+from app.schemas import PostCommentCreate, PostCommentEdit, PostCommentRead, PostCommentWrite
+
+router = APIRouter()
+
+
+@router.get("/", response_model=list[PostCommentRead])
+async def get_post_comments(session: SessionDep, params: Annotated[PostCommentFilterParams, Query()]):
+    db_objs = query.get_post_comments(session=session, params=params)
+    return db_objs
+
+
+# TODO: 리턴 스키마 정의
+@router.post("/")
+async def write_post_comment(session: SessionDep, post_comment_write: PostCommentWrite, user: AuthUserDep):
+    post = query.get_post_by_short_id(session=session, short_id=post_comment_write.short_id)
+    comment_in = PostCommentCreate(**post_comment_write.model_dump(), user_id=user.id, post_id=post.id)
+    db_obj = query.create_post_comment(session=session, comment_in=comment_in)
+    return db_obj
+
+
+# TODO: 리턴 스키마 정의
+@router.patch("/{comment_id}")
+async def edit_post_comment(session: SessionDep, comment_edit: PostCommentEdit, user: AuthUserDep, comment_id: int):
+    comment = query.get_post_comment_by_id(session=session, comment_id=comment_id)
+    if not comment:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+    # 작성자 본인 인지 확인
+    if comment.user_id != user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to edit this post")
+
+    # 1. 클라이언트가 보낸 데이터 중, 값이 설정된 필드만 추출
+    update_data = comment_edit.model_dump(exclude_unset=True)
+
+    # 2. 수정할 내용이 없으면 204 No Content 반환
+    if not update_data:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    # 3. 업데이트 진행
+    comment.update_from_dict(update_data)
+    session.add(comment)
+    session.commit()
+    session.refresh(comment)
+
+    return comment
+
+
+@router.delete("/{comment_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def remove_post_comment(session: SessionDep, user: AuthUserDep, comment_id: int):
+    comment = query.get_post_comment_by_id(session=session, comment_id=comment_id)
+
+    if not comment:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+    # 작성자 본인 인지 확인
+    if comment.user_id != user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to edit this post")
+
+    comment.is_delete = True
+    comment.deleted_at = datetime.now(UTC)
+
+    session.add(comment)
+    session.commit()
