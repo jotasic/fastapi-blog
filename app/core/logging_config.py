34d2f09906a0d9d583a81ae833e938e6d0d6e872
Taewoo Kim <embedded61@gmail.com>
2025-12-24 00:50:37 +0900

    Feat: Correlation ID 도입 및 log 설정 수정 (#36)
    Correlation ID
    - 미들웨어에서 부터 고유 ID를 부여받아 모든 로그에 ID가 나오도록 세팅
    - 로그에서는 request_id로 찍힘
    - 만약 API 요청 시 X-REQUEST-ID 이름의 헤더가 있을 시 해당 ID로 세팅
      - 상세 설정(id 포멧 변경 여부) 가능하여 추후 환경에 따라서 변경 가능
    
    - dev 환경에서 불필요한 filename, funcName, lineno 가 안나오도록 foreign_pre_chain 및  structlog processors 값 환경에 따라서 세팅되도록 수정
    
    - structlog.processors.JSONRenderer serializer orjson.dumps 삭제
      - orjson 사용 시 logger_factory에 structlog.BytesLoggerFactory()를 세팅해야됨
      - 기본 로거에는 세팅할 방법이 없어 기본으로 롤백
      - orjson을 사용안하므로 패키지에서 삭제
    
    - 그외 누락된 uvicorn.error handler 설정 추가

--- app/core/logging_config.py
+++ app/core/logging_config.py
@@ -1,9 +1,10 @@
 # https://www.structlog.org/en/stable/standard-library.html
 
 import logging.config
+from typing import Any
 
-import orjson
 import structlog
+from asgi_correlation_id import correlation_id
 
 from app.config import settings
 
@@ -23,80 +24,56 @@ def extract_from_record(_, __, event_dict):
     return event_dict
 
 
-def setup_logging() -> None:
-    if settings.ENVIRONMENT == "local":
-        root_level = "DEBUG"
-        sql_level = "INFO"
-        uvicorn_level = "INFO"
-        format_type = "console"
-    else:
-        root_level = "INFO"
-        sql_level = "WARNING"
-        uvicorn_level = "INFO"
-        format_type = "json"
+def add_correlation(logger: logging.Logger, method_name: str, event_dict: dict[str, Any]) -> dict[str, Any]:
+    if request_id := correlation_id.get():
+        event_dict["request_id"] = request_id
+    return event_dict
 
+
+def setup_logging() -> None:
     message_key = "message"  # 로그 메세지의 key 값 (기본: event)
 
     # 1. 공통 타임스탬프 설정 (ISO 포맷)
     timestamper = structlog.processors.TimeStamper(fmt="iso")
 
-    # 2. 외부 라이브러리(Standard Logging)가 structlog 포맷터로 들어오기 전 거치는 전처리
-    # structlog.configure(processors=[]) 와 최대한 비슷하게 한다
-    foreign_pre_chain = [
-        structlog.stdlib.add_log_level,
-        structlog.stdlib.add_logger_name,
-        structlog.stdlib.ExtraAdder(),
-        extract_from_record,  # LogRecord에서 위치 정보 추출
-        timestamper,
-    ]
-    logging.config.dictConfig(
-        {
-            "version": 1,
-            # 설정파일에 없는 로거를 비활성화 시킬지 여부
-            "disable_existing_loggers": False,
-            "formatters": {
-                "console": {
-                    "()": structlog.stdlib.ProcessorFormatter,
-                    "processors": [
-                        structlog.stdlib.ProcessorFormatter.remove_processors_meta,
-                        # 콘솔에서는 예외를 줄글로 보는게 편하므로 여기서 변환
-                        structlog.processors.format_exc_info,
-                        structlog.processors.EventRenamer(message_key),
-                        structlog.dev.ConsoleRenderer(colors=True, event_key=message_key),
-                    ],
-                    "foreign_pre_chain": foreign_pre_chain,
-                },
-                "json": {
-                    "()": structlog.stdlib.ProcessorFormatter,
-                    "processors": [
-                        structlog.stdlib.ProcessorFormatter.remove_processors_meta,
-                        structlog.processors.dict_tracebacks,
-                        structlog.processors.EventRenamer(message_key),
-                        structlog.processors.JSONRenderer(serializer=orjson.dumps),
-                    ],
-                    "foreign_pre_chain": foreign_pre_chain,
-                },
-            },
-            "handlers": {
-                "default": {
-                    "level": "DEBUG",
-                    "class": "logging.StreamHandler",
-                    "formatter": format_type,
-                },
-            },
-            "loggers": {
-                "": {"handlers": ["default"], "level": root_level, "propagate": True},
-                "uvicorn": {"handlers": ["default"], "level": uvicorn_level, "propagate": False},
-                "uvicorn.error": {"level": uvicorn_level, "propagate": False},
-                "uvicorn.access": {"handlers": ["default"], "level": uvicorn_level, "propagate": False},
-                "fastapi": {"handlers": ["default"], "level": root_level, "propagate": False},
-                "sqlalchemy.engine": {"handlers": ["default"], "level": sql_level, "propagate": False},
-            },
-        }
-    )
+    if settings.ENVIRONMENT == "local":
+        root_level = logging.DEBUG
+        sql_level = logging.INFO
+        uvicorn_level = logging.INFO
+        format_type = "console"
 
-    structlog.configure(
-        processors=[
+        foreign_pre_chain = [
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.ExtraAdder(),
+            timestamper,
+        ]
+
+        structlog_processors = [
+            # 비동기 환경(FastAPI)에서 ContextVars(Request ID 등) 병합
+            structlog.contextvars.merge_contextvars,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            timestamper,
+            structlog.processors.StackInfoRenderer(),
+            structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        ]
+
+    else:
+        root_level = logging.INFO
+        sql_level = logging.WARNING
+        uvicorn_level = logging.INFO
+        format_type = "json"
+
+        foreign_pre_chain = [
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.ExtraAdder(),
+            timestamper,
+        ]
+
+        structlog_processors = [
             # 비동기 환경(FastAPI)에서 ContextVars(Request ID 등) 병합
             structlog.contextvars.merge_contextvars,
             # 호출 위치 정보 추가 (스택 오염 전 실행)
@@ -113,11 +90,60 @@ def setup_logging() -> None:
             structlog.stdlib.PositionalArgumentsFormatter(),
             timestamper,
             structlog.processors.StackInfoRenderer(),
-            # 중요: format_exc_info를 여기서 제거함!
-            # 이유는 Formatter 단계(console vs json)에서 각각 다르게 처리하기 위함.
-            # 마지막에 반드시 필요 (std logging의 Formatter로 넘김)
             structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
-        ],
+        ]
+
+    # 2. 외부 라이브러리(Standard Logging)가 structlog 포맷터로 들어오기 전 거치는 전처리
+    # structlog.configure(processors=[]) 와 최대한 비슷하게 한다
+
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            # 설정파일에 없는 로거를 비활성화 시킬지 여부
+            "disable_existing_loggers": False,
+            "formatters": {
+                "console": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processors": [
+                        add_correlation,
+                        structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                        structlog.processors.EventRenamer(message_key),
+                        structlog.dev.ConsoleRenderer(colors=True, event_key=message_key),
+                    ],
+                    "foreign_pre_chain": foreign_pre_chain,
+                },
+                "json": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processors": [
+                        add_correlation,
+                        structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                        structlog.processors.dict_tracebacks,
+                        structlog.processors.EventRenamer(message_key),
+                        structlog.processors.JSONRenderer(),
+                    ],
+                    "foreign_pre_chain": foreign_pre_chain,
+                },
+            },
+            "handlers": {
+                "default": {
+                    "level": "DEBUG",
+                    "class": "logging.StreamHandler",
+                    "formatter": format_type,
+                },
+            },
+            "loggers": {
+                "": {"handlers": ["default"], "level": root_level, "propagate": True},
+                "uvicorn": {"handlers": ["default"], "level": uvicorn_level, "propagate": False},
+                "uvicorn.error": {"handlers": ["default"], "level": uvicorn_level, "propagate": False},
+                "uvicorn.access": {"handlers": ["default"], "level": uvicorn_level, "propagate": False},
+                "fastapi": {"handlers": ["default"], "level": root_level, "propagate": False},
+                "sqlalchemy.engine": {"handlers": ["default"], "level": sql_level, "propagate": False},
+            },
+        }
+    )
+
+    structlog.configure(
+        processors=structlog_processors,
         logger_factory=structlog.stdlib.LoggerFactory(),
         wrapper_class=structlog.stdlib.BoundLogger,
         cache_logger_on_first_use=True,
