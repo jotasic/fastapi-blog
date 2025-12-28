1b05396318620e683c6715085e8338674c95c333
taewookim <embedded61@gmail.com>
2025-12-28 15:41:14 +0900

    feat: reate_tabel async 적용 - async 적용 및 해당 함수 이벤트 루프에서 동작하도록 anyio 적용 - 이전 경로에 존재한 빈 파일 삭제

--- scripts/recreate_tables.py
+++ scripts/recreate_tables.py
@@ -2,32 +2,34 @@ import logging
 import os
 import sys
 
+import anyio
+
 # 프로젝트 루트 경로를 sys.path에 추가하여 app 모듈을 찾을 수 있게 함
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from app.core.database import engine
-from app.models import BaseModel
+from app.core.database import async_engine
+from app.core.logging_config import setup_logging
+from app.models.base import BaseModel
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 
 
-def main():
+async def main():
     confirm = input("recreate tables? (y/n): ")
     if confirm.lower() != "y":
         logger.info("Cancelled.")
-        return
 
     logger.info("Start")
 
-    with engine.begin() as conn:
+    async with async_engine.begin() as conn:
         logger.info("Drop tables")
-        BaseModel.metadata.drop_all(conn)
+        await conn.run_sync(BaseModel.metadata.drop_all)
         logger.info("Create tables")
-        BaseModel.metadata.create_all(conn)
+        await conn.run_sync(BaseModel.metadata.create_all)
 
     logger.info("Done.")
 
 
 if __name__ == "__main__":
-    main()
+    setup_logging()
+    anyio.run(main)
